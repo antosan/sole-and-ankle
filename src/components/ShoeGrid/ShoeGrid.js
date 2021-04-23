b87467234fe569cdca14a30d97722d77df67be3f
Antony Sande <sandeantony@gmail.com>
2021-04-23 22:09:01 +0300

    feat: grid layout

--- src/components/ShoeGrid/ShoeGrid.js
+++ src/components/ShoeGrid/ShoeGrid.js
@@ -8,12 +8,23 @@ const ShoeGrid = () => {
   return (
     <Wrapper>
       {SHOES.map((shoe) => (
-        <ShoeCard key={shoe.slug} {...shoe} />
+        <ShoeWrapper key={shoe.slug}>
+          <ShoeCard {...shoe} />
+        </ShoeWrapper>
       ))}
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 36px;
+`;
+
+const ShoeWrapper = styled.div`
+  min-width: 300px;
+  flex: 1;
+`;
 
 export default ShoeGrid;
